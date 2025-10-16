b27b9221b7f6247a791cbb9b7ffe639406a85a68
mdsharia4613 <mdsharia4613@gmail.com>
2025-10-16 22:31:02 +0600

    project-setup

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Navber from './Components/Navber/Navber'
+
+function App() {
+ 
+
+  return (
+    <>
+      <Navber></Navber>
+     
+      
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Navber/Navber.jsx
@@ -0,0 +1,21 @@
+
+
+const Navber = () => {
+    return (
+        <div className='bg-slate-200'>
+            <div className='flex justify-between items-center container mx-auto py-5 px-2 '>
+                <div className="text-3xl font-semibold">
+                    <h2>Logo</h2>
+                </div>
+                <div className="flex gap-4 ">
+                    <a href="">Home</a>
+                    <a href="">Product</a>
+                    <a href="">card 1</a>
+                    <p>$ 5.00</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navber;
